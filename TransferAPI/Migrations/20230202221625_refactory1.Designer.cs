e7d6d6568e874b59b6eef5b2095d65ec0cecde86
Francisco Jefferson da Silva Rodrigues <jsilvardg@outlook.com>
2023-02-02 20:50:17 -0300

    Refatoração de entidades

--- TransferAPI/Migrations/20230202221625_refactory1.Designer.cs
+++ TransferAPI/Migrations/20230202221625_refactory1.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TransferAPI.Data;
 
@@ -11,9 +12,11 @@ using TransferAPI.Data;
 namespace TransferAPI.Migrations
 {
     [DbContext(typeof(TransferContext))]
-    partial class TransferContextModelSnapshot : ModelSnapshot
+    [Migration("20230202221625_refactory1")]
+    partial class refactory1
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- TransferAPI/Models/User.cs
+++ TransferAPI/Models/User.cs
@@ -7,8 +7,11 @@ namespace TransferAPI.Models
     {
         public int Id { get; set; }
         [MaxLength(200)]
+        public string FullName { get; set; }
+        [MaxLength(100)]
+        public string Email { get; set; }
+        [MaxLength(200)]
         public string Username { get; set; }
-        [PasswordPropertyText]
         [MaxLength(100)]
         public string Password { get; set; }
         public Account? Account { get; set; } = new Account();
