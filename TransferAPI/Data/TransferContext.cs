a01d29687cd5e1b2573953b8dd41c7259be6b150
Francisco Jefferson da Silva Rodrigues <jsilvardg@outlook.com>
2023-02-01 21:49:06 -0300

    Configurando contexto da aplicação

---
+++ TransferAPI/Data/TransferContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TransferAPI.Models;
+
+namespace TransferAPI.Data
+{
+    public class TransferContext : DbContext
+    {
+        public TransferContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+
+    }
+}
--- TransferAPI/Program.cs
+++ TransferAPI/Program.cs
@@ -1,11 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using TransferAPI.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+// Adição de contexto da aplicação
+builder.Services.AddDbContext<TransferContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("default")));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c => c.SwaggerDoc("v1", new OpenApiInfo { Title = "API de transferências", Version = "v1" }));
 
 var app = builder.Build();
 
